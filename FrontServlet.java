b700e55a297db24288e2f74b938c16257231a913
Seesiz <108148026+Seesiz@users.noreply.github.com>
2023-06-29 20:35:39 +0300

    maj sprint 1

---
+++ FrontServlet.java
@@ -0,0 +1,35 @@
+package etu1932.framework.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.*;
+import java.lang.reflect.Field;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FrontServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(req.getRequestURL());
+        out.println(UrlMapping);
+        processRequest(req, res);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        res.setContentType("text/plain");
+        PrintWriter out = res.getWriter();
+        out.println(req.getRequestURL());
+        processRequest(req, res);
+    }
+}
